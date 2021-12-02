0da3eeea7a773dbeb3248ac892a8c60758879079
yothebob <b.cmoderndesigns@gmail.com>
2021-12-01 20:24:08 -0800

    worked on CLI menu

--- cli/main.py
+++ cli/main.py
@@ -4,6 +4,7 @@ from cli.app import CliApp
 app = CliApp()
 
 def home_page():
+    '''welcome screen function and login/create user functionality'''
     print("""
         SIMPLE HOME THEATER
 
@@ -18,8 +19,19 @@ def home_page():
         return app.login()
 
 
+def show_user_categories(user_categories):
+    print("user categories:")
+    [print(index,": ",user_categories[index].name) for index in range(len(user_categories))]
+    print("type a number to pick a category:\n")
+    user_input = input(": ")
+    return int(user_input)
+
+
 def run_app():
+    user = home_page()
+    user_categories = user.load_categories()
+    picked_category = user_categories[show_user_categories(user_categories)]
+    print(picked_category.load_category_contents())
 
-    instance = app.login()
-
-category_contents[0].play_content()
+run_app()
+# category_contents[0].play_content()

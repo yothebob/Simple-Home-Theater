90eda1b605a2dfde27d1bf7dae4d101da9cb8389
yothebob <b.cmoderndesigns@gmail.com>
2021-12-11 11:41:39 -0800

    master-csv: write a one line write query for orm.py (in main.py)

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import core.core_settings as settings
-import fileinput
 
 #run this file to run the program of your choice
 # if settings.DEPLOYMENT_TYPE == "CLI":
@@ -7,18 +6,18 @@ import fileinput
 # elif settings.DEPLOYMENT_TYPE == "WEB":
 #     import webapp.main
 
-# writes on top of the line
-# f = open("/home/brandon/Documents/python-projects/simple-home-theater/data/categories.csv", "r")
-# contents = f.readlines()
-# f.close()
-#
-# contents.insert(index, "1,1,Music,brandon,/home/brandons/Music, testing")
-#
-# f = open("/home/brandon/Documents/python-projects/simple-home-theater/data/categories.csv", "w")
-# contents = "".join(contents)
-# f.write(contents)
-# f.close()
 
-for line in fileinput.FileInput("/home/brandon/Documents/python-projects/simple-home-theater/data/categories.csv",inplace=1):
-    if "2" in line:
-        line = line.replace(line,"1,1,Music,brandon,/home/brandons/Music, testing")
+
+#write_query one line
+
+db_rows = [row for row in open("/home/brandon/Documents/python-projects/simple-home-theater/data/categories.csv", "r")]
+print(db_rows)
+
+f = open("/home/brandon/Documents/python-projects/simple-home-theater/data/categories.csv", "w")
+for row in db_rows:
+    if "2" in row:
+        new_line = "1,1,Music,brandon,/home/brandons/Music, testing\n"
+        f.write(new_line)
+    else:
+        f.write(row)
+f.close()

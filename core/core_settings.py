805f64b7d088ae030750e62904f410eb120cf874
brandon <b.brodrick96@hotmail.com>
2022-01-25 20:32:35 -0800

    fixed coresetting conflict

--- core/core_settings.py
+++ core/core_settings.py
@@ -1,18 +1,13 @@
 PROJECT_FILEPATH = "/home/brandon/Documents/python-projects/simple-home-theater"
 MEDIA_PLAYER = "mpv"
-# DEPLOYMENT_TYPE = "CLI"
-DEPLOYMENT_TYPE = "WEB"
+DEPLOYMENT_TYPE = "CLI"
+# DEPLOYMENT_TYPE = "WEB"
 APP_NAME = "Simple Home Theater"
 AUTOPLAY_COUNTDOWN = 3#30
 CONTENT_FILETYPES = ["mp4","mp3","opus"]
 
 METADATA_LIST = ["plot","rating","runtimes"] #get these from imdbpy, there is a list in movie_scraper/main.py
 
-<<<<<<< HEAD
-PASSWORD_HASH = False
-=======
-PASSWORD_HASH = False 
->>>>>>> refs/remotes/origin/master-stable
 
 USER_TABLE =  PROJECT_FILEPATH + "/data/users.csv"
 CATEGORY_TABLE = PROJECT_FILEPATH + "/data/categories.csv"
